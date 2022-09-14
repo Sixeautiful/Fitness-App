94cf42fd4023410119d763484420cfa42b53bff0
Sixeautiful <vladerastatkevich2015@gmail.com>
2022-09-14 21:00:12 +0300

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Kirill Willer on 14.09.22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
